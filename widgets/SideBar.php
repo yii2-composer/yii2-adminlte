f7dc5a18531bae54f400f6568a1a3fa74ede1466
liyifei <119422342@qq.com>
2018-10-02 15:21:52 +0800

    优化

--- widgets/SideBar.php
+++ widgets/SideBar.php
@@ -5,6 +5,7 @@
  * Date: 15/12/20
  * Time: 14:09
  */
+
 namespace liyifei\adminlte\widgets;
 
 use yii\base\InvalidConfigException;
@@ -12,7 +13,8 @@ use yii\base\Widget;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
-class SideBar extends Widget{
+class SideBar extends Widget
+{
     /**
      * @var array list of items in the nav widget. Each array element represents a single
      * menu item which can be either a string or an array with the following structure:
@@ -32,58 +34,71 @@ class SideBar extends Widget{
     public $actives = [];
     public $encodeLabels = true;
     public $params;
-    
-    public function run(){
-        if(isset($this->items['actives']))
+
+    public function init()
+    {
+        foreach ($this->items as $key => $item) {
+            $this->_putActives($route = \Yii::$app->controller->route, $item, $key, []);
+        }
+
+        parent::init();
+    }
+
+    public function run()
+    {
+        if (isset($this->items['actives']))
             unset($this->items['actives']);
         return $this->renderItems();
     }
-    
-    public function renderItems(){
-        $items=[];
-        foreach($this->items as $key=>$item){
+
+    public function renderItems()
+    {
+        $items = [];
+        foreach ($this->items as $key => $item) {
             if (isset($item['visible']) && !$item['visible']) {
                 continue;
             }
-            if(in_array($key,$this->actives))
-                $item['active']=true;
+            if (in_array($key, $this->actives))
+                $item['active'] = true;
             $items[] = $this->renderItem($item);
         }
-        
-        return Html::tag('ul',implode("\n",$items),['class'=>'sidebar-menu']);
+
+        return Html::tag('ul', implode("\n", $items), ['class' => 'sidebar-menu']);
     }
-    
-    public function renderItem($item){
-        if(is_string($item))
+
+    public function renderItem($item)
+    {
+        if (is_string($item))
             return $item;
         if (!isset($item['label'])) {
             throw new InvalidConfigException("The 'label' option is required.");
         }
         $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
         $label = $encodeLabel ? Html::encode($item['label']) : $item['label'];
-        $icon = isset($item['icon'])?Html::tag('i','',['class'=>$item['icon']]):'';
-        $label = $icon.Html::tag('span', $label);
+        $icon = isset($item['icon']) ? Html::tag('i', '', ['class' => $item['icon']]) : '';
+        $label = $icon . Html::tag('span', $label);
         $options = ArrayHelper::getValue($item, 'options', []);
         $items = ArrayHelper::getValue($item, 'items');
         $url = ArrayHelper::getValue($item, 'url', '#');
         $linkOptions = ArrayHelper::getValue($item, 'linkOptions', []);
-        $active =ArrayHelper::getValue($item,'active',false);
-        if($active)
-            Html::addCssClass($options,'active');
+        $active = ArrayHelper::getValue($item, 'active', false);
+        if ($active)
+            Html::addCssClass($options, 'active');
         if ($items !== null && is_array($items)) {
-            $url='#';
-            $label=$label.'<i class="fa fa-angle-left pull-right"></i>';
-            $items = $this->renderSubItems($items,$item);
-        }else{
-            $items='';
+            $url = '#';
+            $label = $label . '<i class="fa fa-angle-left pull-right"></i>';
+            $items = $this->renderSubItems($items, $item);
+        } else {
+            $items = '';
         }
 
-        return Html::tag('li', Html::a($label, $url, $linkOptions). $items , $options);
+        return Html::tag('li', Html::a($label, $url, $linkOptions) . $items, $options);
     }
-    
-    protected function renderSubItems($items,$parentItem){
+
+    protected function renderSubItems($items, $parentItem)
+    {
         $lines = [];
-        foreach ($items as $key=>$item) {
+        foreach ($items as $key => $item) {
             if (isset($item['visible']) && !$item['visible']) {
                 continue;
             }
@@ -97,20 +112,19 @@ class SideBar extends Widget{
             $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
             $label = $encodeLabel ? Html::encode($item['label']) : $item['label'];
             $icon = isset($item['icon']) ? Html::tag('i', '', ['class' => $item['icon']]) : '';
-            $label = $icon.Html::tag('span', $label);
+            $label = $icon . Html::tag('span', $label);
             $itemOptions = ArrayHelper::getValue($item, 'options', []);
             $url = ArrayHelper::getValue($item, 'url', '#');
             $linkOptions = ArrayHelper::getValue($item, 'linkOptions', []);
             $active = ArrayHelper::getValue($item, 'active', false);
-            if ($active || in_array($key,$this->actives))
+            if ($active || in_array($key, $this->actives))
                 Html::addCssClass($itemOptions, 'active');
             $subitems = ArrayHelper::getValue($item, 'items');
             if ($subitems !== null && is_array($subitems)) {
-                $url='#';
+                $url = '#';
                 $label = $label . '<i class="fa fa-angle-left pull-right"></i>';
                 $subitems = $this->renderSubItems($subitems, $item);
-            }
-            else {
+            } else {
                 $subitems = '';
             }
 
@@ -118,5 +132,20 @@ class SideBar extends Widget{
         }
 
         return Html::tag('ul', implode("\n", $lines), ['class' => 'treeview-menu']);
+    }
+
+    private function _putActives($route, $current, $currentKey, $keys = [])
+    {
+        if (isset($current['items']) && $current['items']) {
+            $keys[] = $currentKey;
+            foreach ($current['items'] as $key => $item) {
+                $this->_putActives($route, $item, $key, $keys);
+            }
+        } else {
+            if (strpos($route, trim($current['url'][0], '/')) === 0) {
+                $keys[] = $currentKey;
+                $this->actives = $keys;
+            }
+        }
     }
 }
